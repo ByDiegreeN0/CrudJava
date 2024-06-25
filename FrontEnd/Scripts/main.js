50be40d03d488c9dbc8f54461287f7bf06a0a465
anarkemos <anarkemos@gmail.com>
2024-06-25 09:07:28 -0500

    Log-in listo

--- FrontEnd/Scripts/main.js
+++ FrontEnd/Scripts/main.js
@@ -1,2 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previene el envío del formulario
+
+    const password1 = document.getElementById('InputPassword1').value;
+    const password2 = document.getElementById('InputPassword2').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (password1 !== password2) {
+        errorMessage.style.display = 'block';
+    } else {
+        errorMessage.style.display = 'none';
+    }
+});
